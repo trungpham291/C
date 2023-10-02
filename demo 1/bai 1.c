a53ebc0e27fa8762d9e496d10d75fe7a90e98874
trungpham291 <trungpham29103@gmail.com>
2023-10-02 18:25:43 +0700

    Hello C

--- demo 1/bai 1.c
+++ demo 1/bai 1.c
@@ -1,53 +1,18 @@
 #include <stdio.h>
 
-#define COFFEE_PRICE 2.5
-#define TEA_PRICE 2.0
-#define SMOOTHIE_PRICE 3.0
-int main1()
-//int main()
+int main()
 {
-    int numCoffees =0, numTeas = 0, numSmoothies = 0;
-    float totalCost;
-    char choice;
-
-    printf("welcome to coffee shop! \n");
-
-    do{
-        printf("\n-----MENU------\n");
-        printf("1. Coffee ($%.2f)\n", COFFEE_PRICE);
-        printf("2. Tea ($%.2f)\n", TEA_PRICE);
-        printf("3. Smoothie ($%.2f)\n", SMOOTHIE_PRICE);
-        printf("4. Quit\n");
-
-        printf("Enter your choice (1-4): ");
-        scanf("%c", &choice);
-
-        switch (choice) {
-            case '1':
-                numCoffees++;
-                break;
-            case '2':
-                numTeas++;
-                break;
-            case '3':
-                numSmoothies++;
-                break;
-            case '4':
-                break;
-            default:
-                printf("Invalid choice. Please try again. \n");
-        }
-
-    }while (choice != '4');
-
-    totalCost = (numCoffees * COFFEE_PRICE) + (numTeas * TEA_PRICE) +(numSmoothies * SMOOTHIE_PRICE);
-
-    printf("\n Order Details: \n");
-    printf("Coffees: %d\n", numCoffees);
-    printf("Teas: %d\n", numTeas);
-    printf("Smoothies: %d\n", numSmoothies);
-    printf("Total Cost: $%.2f\n", totalCost);
-
-    return 0;
-
+    int i;
+    int arr[7];
+    arr[0] = 1;
+    arr[1] = 3;
+    arr[2] = 7;
+    arr[3] = 9;
+    arr[4] = 4;
+    arr[5] = 6;
+    arr[6] = 5;
+    for (i=0; i<7; i++)
+    {
+        printf("%d \n", arr[i]);
+    }
 }
